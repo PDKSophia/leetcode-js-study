3a75a1a2133f437e043653ec425c3f110cd9c983
DK_Peng <1063137960@qq.com>
2018-12-07 19:05:38 +0800

    feat: 新题

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
- var removeElement = function(nums, val) {
-    let len = 0
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] !== val) {
-        nums[len++] = nums[i]
-      }
+  var rob = function(nums) {
+    if (nums.length === 0) return 0
+    if (nums.length === 1) return nums[0]
+    var maxMoney = [nums[0], Math.max(nums[0], nums[1])]
+
+    for (let i = 2; i < nums.length; i++) {
+      maxMoney.push(Math.max(nums[i] + maxMoney[i-2], maxMoney[i-1]))
     }
-    console.log(nums)
-    return len
-  }
-  var nums = [3, 2, 2, 3]
-  var res = removeElement(nums, 3)
-  console.log(res);
+    return maxMoney[maxMoney.length -1]
+  }; 
+
+  var arr = [2,7,9,3,1]
+  console.log(rob(arr))
